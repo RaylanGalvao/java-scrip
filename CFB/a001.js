5734b6cfaa4afb3d5f864c36d8e349b7696659a0
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-02-20 14:20:18 -0300

    primeiros passos de java script

---
+++ CFB/a001.js
@@ -0,0 +1,11 @@
+//console.log("esta linha esta comentada")
+console.log("ola mundo")
+/*console.log("ola mundo")
+console.log("ola mundo")
+console.log("ola mundo")*/
+/*console.log ("esse e um comentario de bloco")*/ 
+console.log("ola mundo")
+console.log("ola mundo")
+console.log("ola mundo")
+console.log("ola mundo")
+
