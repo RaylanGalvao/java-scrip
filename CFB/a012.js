4d3472aa05cfe8ca7c052907345116752aa37623
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-02-26 14:17:21 -0300

    exercicio de js

--- CFB/a012.js
+++ CFB/a012.js
@@ -10,8 +10,21 @@
 
 // console.log(jogador3)
 
-const soma=(v1, v2, v3)=>{
-    return v1+v2+v3
-}
+const objs1=document.getElementsByTagName("div")
+const objs2=[...document.getElementsByTagName("div")]
 
-console.log(soma(1,5,4))
+objs2.forEach(element => {
+    element.innerHTML="curso"
+});
+
+console.log(objs1)
+console.log(objs2)
+
+// const soma=(v1, v2, v3)=>{
+//     return v1+v2+v3
+// }
+
+// let valores=[1,15,4]
+
+// console.log(soma(...valores))
+
